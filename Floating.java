ab040e4ed093f11fc4ed569190d4717efcf45154
mojokification <animesh_johnny@yahoo.com>
2017-12-26 22:34:14 +0530

    Added .java file

---
+++ Floating.java
@@ -0,0 +1,15 @@
+import java.lang.System;
+public class Floating{
+	public static void main (String args[]){
+	
+	Float f=(float)4.5;
+	System.out.println("Floating point number is"+f);
+	Double d=10.7;
+	System.out.println("Double number is"+d);
+	Boolean b=true;
+	System.out.println("Boolean value is "+b);
+	
+
+
+	}
+}
